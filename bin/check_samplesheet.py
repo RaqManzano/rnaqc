018c8181c60415fb51b45382c81c41f8a0149a4b
Raquel Manzano <rm889@cam.ac.uk>
2024-01-29 14:03:26 +0000

    Changed input_check for BAMs

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -25,16 +25,16 @@ class RowChecker:
     """
 
     VALID_FORMATS = (
-        ".fq.gz",
-        ".fastq.gz",
+        ".bam",
+        ".bai",
     )
 
     def __init__(
         self,
         sample_col="sample",
-        first_col="fastq_1",
-        second_col="fastq_2",
-        single_col="single_end",
+        first_col="bam",
+        second_col="bai",
+        index_col="indexed",
         **kwargs,
     ):
         """
@@ -44,19 +44,19 @@ class RowChecker:
             sample_col (str): The name of the column that contains the sample name
                 (default "sample").
             first_col (str): The name of the column that contains the first (or only)
-                FASTQ file path (default "fastq_1").
+                BAM file path (default "bam").
             second_col (str): The name of the column that contains the second (if any)
-                FASTQ file path (default "fastq_2").
-            single_col (str): The name of the new column that will be inserted and
+                BAI file path (default "bai").
+            index_col (str): The name of the new column that will be inserted and
                 records whether the sample contains single- or paired-end sequencing
-                reads (default "single_end").
+                reads (default "indexed").
 
         """
         super().__init__(**kwargs)
         self._sample_col = sample_col
         self._first_col = first_col
         self._second_col = second_col
-        self._single_col = single_col
+        self._index_col = index_col
         self._seen = set()
         self.modified = []
 
@@ -84,45 +84,41 @@ class RowChecker:
         row[self._sample_col] = row[self._sample_col].replace(" ", "_")
 
     def _validate_first(self, row):
-        """Assert that the first FASTQ entry is non-empty and has the right format."""
+        """Assert that the first BAM entry is non-empty and has the right format."""
         if len(row[self._first_col]) <= 0:
-            raise AssertionError("At least the first FASTQ file is required.")
-        self._validate_fastq_format(row[self._first_col])
+            raise AssertionError("At least the first BAM file is required.")
+        self._validate_BAM_format(row[self._first_col])
 
     def _validate_second(self, row):
-        """Assert that the second FASTQ entry has the right format if it exists."""
+        """Assert that the second BAM entry has the right format if it exists."""
         if len(row[self._second_col]) > 0:
-            self._validate_fastq_format(row[self._second_col])
+            self._validate_BAM_format(row[self._second_col])
 
     def _validate_pair(self, row):
         """Assert that read pairs have the same file extension. Report pair status."""
         if row[self._first_col] and row[self._second_col]:
-            row[self._single_col] = False
-            first_col_suffix = Path(row[self._first_col]).suffixes[-2:]
-            second_col_suffix = Path(row[self._second_col]).suffixes[-2:]
-            if first_col_suffix != second_col_suffix:
-                raise AssertionError("FASTQ pairs must have the same file extensions.")
+            row[self._index_col] = True
         else:
-            row[self._single_col] = True
+            row[self._index_col] = False
 
-    def _validate_fastq_format(self, filename):
-        """Assert that a given filename has one of the expected FASTQ extensions."""
+    def _validate_BAM_format(self, filename):
+        """Assert that a given filename has one of the expected BAM extensions."""
         if not any(filename.endswith(extension) for extension in self.VALID_FORMATS):
             raise AssertionError(
-                f"The FASTQ file has an unrecognized extension: {filename}\n"
+                f"The BAM file has an unrecognized extension: {filename}\n"
                 f"It should be one of: {', '.join(self.VALID_FORMATS)}"
             )
 
     def validate_unique_samples(self):
         """
-        Assert that the combination of sample name and FASTQ filename is unique.
+        Assert that the combination of sample name and BAM filename is unique.
 
         In addition to the validation, also rename all samples to have a suffix of _T{n}, where n is the
-        number of times the same sample exist, but with different FASTQ files, e.g., multiple runs per experiment.
+        number of times the same sample exist, but with different BAM files, e.g., multiple runs per experiment.
 
         """
         if len(self._seen) != len(self.modified):
-            raise AssertionError("The pair of sample name and FASTQ must be unique.")
+            raise AssertionError("The pair of sample name and BAM must be unique.")
         seen = Counter()
         for row in self.modified:
             sample = row[self._sample_col]
@@ -167,7 +163,7 @@ def check_samplesheet(file_in, file_out):
     Check that the tabular samplesheet has the structure expected by nf-core pipelines.
 
     Validate the general shape of the table, expected columns, and each row. Also add
-    an additional column which records whether one or two FASTQ reads were found.
+    an additional column which records whether one or two BAM reads were found.
 
     Args:
         file_in (pathlib.Path): The given tabular samplesheet. The format can be either
@@ -179,16 +175,16 @@ def check_samplesheet(file_in, file_out):
         This function checks that the samplesheet follows the following structure,
         see also the `viral recon samplesheet`_::
 
-            sample,fastq_1,fastq_2
-            SAMPLE_PE,SAMPLE_PE_RUN1_1.fastq.gz,SAMPLE_PE_RUN1_2.fastq.gz
-            SAMPLE_PE,SAMPLE_PE_RUN2_1.fastq.gz,SAMPLE_PE_RUN2_2.fastq.gz
-            SAMPLE_SE,SAMPLE_SE_RUN1_1.fastq.gz,
+            sample,bam,bai
+            SAMPLE_PE,SAMPLE_PE_RUN1_1.BAM.gz,SAMPLE_PE_RUN1_2.BAM.gz
+            SAMPLE_PE,SAMPLE_PE_RUN2_1.BAM.gz,SAMPLE_PE_RUN2_2.BAM.gz
+            SAMPLE_SE,SAMPLE_SE_RUN1_1.BAM.gz,
 
     .. _viral recon samplesheet:
         https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
 
     """
-    required_columns = {"sample", "fastq_1", "fastq_2"}
+    required_columns = {"sample", "bam", "bai"}
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_in.open(newline="") as in_handle:
         reader = csv.DictReader(in_handle, dialect=sniff_format(in_handle))
@@ -207,7 +203,7 @@ def check_samplesheet(file_in, file_out):
                 sys.exit(1)
         checker.validate_unique_samples()
     header = list(reader.fieldnames)
-    header.insert(1, "single_end")
+    header.insert(1, "indexed")
     # See https://docs.python.org/3.9/library/csv.html#id3 to read up on `newline=""`.
     with file_out.open(mode="w", newline="") as out_handle:
         writer = csv.DictWriter(out_handle, header, delimiter=",")
--- subworkflows/local/input_check.nf
+++ subworkflows/local/input_check.nf
@@ -12,7 +12,7 @@ workflow INPUT_CHECK {
     SAMPLESHEET_CHECK ( samplesheet )
         .csv
         .splitCsv ( header:true, sep:',' )
-        .map { create_fastq_channel(it) }
+        .map { create_bam_channel(it) }
         .set { reads }
 
     emit:
@@ -21,24 +21,24 @@ workflow INPUT_CHECK {
 }
 
 // Function to get list of [ meta, [ fastq_1, fastq_2 ] ]
-def create_fastq_channel(LinkedHashMap row) {
+def create_bam_channel(LinkedHashMap row) {
     // create meta map
     def meta = [:]
-    meta.id         = row.sample
-    meta.single_end = row.single_end.toBoolean()
+    meta.id  = row.sample
+    meta.indexed  = row.indexed  == "True"
 
     // add path(s) of the fastq file(s) to the meta map
-    def fastq_meta = []
-    if (!file(row.fastq_1).exists()) {
-        exit 1, "ERROR: Please check input samplesheet -> Read 1 FastQ file does not exist!\n${row.fastq_1}"
+    def bam_meta = []
+    if (!file(row.bam).exists()) {
+        exit 1, "ERROR: Please check input samplesheet -> Read 1 FastQ file does not exist!\n${row.bam}"
     }
-    if (meta.single_end) {
-        fastq_meta = [ meta, [ file(row.fastq_1) ] ]
-    } else {
-        if (!file(row.fastq_2).exists()) {
-            exit 1, "ERROR: Please check input samplesheet -> Read 2 FastQ file does not exist!\n${row.fastq_2}"
+    if (meta.indexed) {
+        if (!file(row.bai).exists()) {
+            exit 1, "ERROR: Please check input samplesheet -> Read 2 FastQ file does not exist!\n${row.bai}"
         }
-        fastq_meta = [ meta, [ file(row.fastq_1), file(row.fastq_2) ] ]
+        bam_meta = [ meta, [ file(row.bam), file(row.bai) ] ]
+    } else {
+        bam_meta = [ meta, [ file(row.bam) ] ]
     }
-    return fastq_meta
+    return bam_meta
 }
